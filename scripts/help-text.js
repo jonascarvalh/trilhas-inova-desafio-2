343c8db06f0740a319e4189ff82306d2d5a9b0c5
mariadelmondes <mdelmondes98m@gmail.com>
2025-03-27 20:05:43 -0300

    feat: adicionando mensagem de item obrigatório nos campos do form

--- scripts/help-text.js
+++ scripts/help-text.js
@@ -22,3 +22,49 @@ form.addEventListener('submit', function(event) {
     }
 
 });
+
+    let dateInput = fields[1].querySelector('#data_nascimento');
+    let helpText = fields[1].getElementsByClassName('help-text')[0];
+
+    if (dateInput.value.trim() === '') {
+        helpText.classList.remove('help__text--hidden');
+        formValido = false;
+    } else {
+        helpText.classList.add('help__text--hidden');
+        formValido = true;
+    }
+
+    if (formValido) {
+        this.submit;
+    }
+
+
+    let cpfInput = fields[2].querySelector('#cpf');
+    let helpText = fields[2].getElementsByClassName('help-text')[0];
+
+    if (cpfInput.value.trim() === '') {
+        helpText.classList.remove('help__text--hidden');
+        formValido = false;
+    } else {
+        helpText.classList.add('help__text--hidden');
+        formValido = true;
+    }
+
+    if (formValido) {
+        this.submit;
+    }
+
+
+    let sexoInput = fields[3].querySelector('#sexo');
+    let helpText = fields[3].getElementsByClassName('help-text')[0];
+
+    if (sexoInput.value.trim() === '') {
+        helpText.classList.remove('help__text--hidden');
+        formValido = false;
+    } else {
+        sexoHelpText.classList.add('help__text--hidden');
+    }
+
+    if (formValido) {
+        this.submit;
+    }
